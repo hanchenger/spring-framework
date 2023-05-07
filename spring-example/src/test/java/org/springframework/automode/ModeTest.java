34912d6ac1bcb728c2a0d1098dc4aecddb6dc583
瀚宸 <m13699236245@163.com>
2023-05-07 16:22:42 +0800

    supplier的应用

--- spring-example/src/test/java/org/springframework/automode/ModeTest.java
+++ spring-example/src/test/java/org/springframework/automode/ModeTest.java
@@ -3,8 +3,14 @@ package org.springframework.automode;
 
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.support.GenericBeanDefinition;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.example.statics.A;
 import org.springframework.example.statics.Config;
+import org.springframework.example.statics.ObjectFactory;
+import org.springframework.example.statics.SupplierFactory;
+
+import java.util.function.Supplier;
 
 @Slf4j
 public class ModeTest {
@@ -20,7 +26,77 @@ public class ModeTest {
 	@Test
 	public  void staticsModel()  {
 		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
-		context.register(Config.class);
+		//context.register(Config.class);
+
+        //beanDefinition称为bean的原料
+		GenericBeanDefinition beanDefinition = new GenericBeanDefinition();
+		beanDefinition.setBeanClass(A.class);
+
+		context.registerBeanDefinition("a",beanDefinition);
+
 		context.refresh();
+	}
+
+	@Test
+	public  void staticsDefinitionModel2()  {
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+		context.register(ObjectFactory.class);
+
+		//beanDefinition称为bean的原料
+		GenericBeanDefinition beanDefinition = new GenericBeanDefinition();
+		beanDefinition.setBeanClass(A.class);
+		beanDefinition.setFactoryBeanName("objectFactory");
+		beanDefinition.setFactoryMethodName("instanceObject");
+
+		context.registerBeanDefinition("a",beanDefinition);
+
+		context.refresh();
+	}
+
+	@Test
+	public  void staticsDefinitionModel3()  {
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+		context.register(ObjectFactory.class);
+
+		//beanDefinition称为bean的原料
+		GenericBeanDefinition beanDefinition = new GenericBeanDefinition();
+		beanDefinition.setBeanClass(A.class);
+		SupplierFactory supplierFactory = new SupplierFactory();
+		beanDefinition.setInstanceSupplier(supplierFactory::instanceObject);
+
+		context.registerBeanDefinition("a",beanDefinition);
+		context.refresh();
+	}
+
+	@Test
+	public void staticsDefinitionModel5() {
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+		context.register(ObjectFactory.class);
+
+		//beanDefinition称为bean的原料
+		GenericBeanDefinition beanDefinition = new GenericBeanDefinition();
+		beanDefinition.setBeanClass(A.class);
+		beanDefinition.setInstanceSupplier(SupplierFactory::getObject);
+
+		context.registerBeanDefinition("a", beanDefinition);
+		context.refresh();
+	}
+
+	@Test
+	public void staticsDefinitionModel6() {
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+		context.register(ObjectFactory.class);
+
+		//beanDefinition称为bean的原料
+		GenericBeanDefinition beanDefinition = new GenericBeanDefinition();
+		beanDefinition.setBeanClass(A.class);
+
+		beanDefinition.setFactoryBeanName("objectFactory");
+		beanDefinition.setFactoryMethodName("instanceObject");
+		beanDefinition.setInstanceSupplier(SupplierFactory::getObject);
+
+		context.registerBeanDefinition("a", beanDefinition);
+		context.refresh();
+
 	}
 }
