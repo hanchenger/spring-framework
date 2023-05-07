ab936cbede13233692a8d151a34d7d1892b9b9c6
瀚宸 <m13699236245@163.com>
2023-05-07 18:15:42 +0800

    spring推断构造方法会采用参数最多的构造方法 虽然这个构造方法参数最多的，但是验证之后，推断构造方法采用的是A(B b,M m)，这是因为String s这个参数不在spring的容器内

--- spring-example/src/main/java/org/springframework/example/defaults/A.java
+++ spring-example/src/main/java/org/springframework/example/defaults/A.java
@@ -22,6 +22,13 @@ public class A implements ExampleAware {
 		log.debug("Constructor from b m");
 	}
 
+	//spring推断构造方法会采用参数最多的构造方法,
+	//虽然这个构造方法参数最多的，但是验证之后，推断构造方法采用的是A(B b,M m)，这是因为String s这个参数不在spring的容器内
+	public A(B b,M m,String s) {
+		log.debug("Constructor from b m s");
+	}
+
+
 
 	//改变A(B b)和A(C c)方法的顺序发现spring推断构造方法采用了到了 A(C c)
 	//这里是按照加载构造方法的顺序的，但是它加载完了之后，就倒过来，并且通过算法会给A(C c)和A(B b)计算一个得分，最后推断构造方法的
