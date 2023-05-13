efc24da1f3d521e72d51bd75794bc4e4ac82bd98
瀚宸 <m13699236245@163.com>
2023-05-13 18:04:06 +0800

    xml方式手动注入:通过autowire="byType"实现自动注入

--- spring-example/src/main/java/org/springframework/example/defaults/M.java
+++ spring-example/src/main/java/org/springframework/example/defaults/M.java
@@ -4,27 +4,26 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-//@Component("mm")
+@Component("mm")
 @Slf4j(topic = "e")
 public class M {
 
-	public M(){
+	public M() {
 		log.debug("default constructor m");
 	}
 
-	public M(N n){
-		log.debug("Constructor N:{}",n);
+	public M(N n) {
+		log.debug("Constructor N:{}", n);
 	}
 
 
-	@Autowired
 	private N n;
 
-	public void setN(N n){
-		log.debug("setter N:{}",n);
+	public void setN(N n) {
+		log.debug("setter N:{}", n);
 	}
 
-	public void setA(A a){
+	public void setA(A a) {
 		log.debug("log a");
 	}
 }
--- spring-example/src/main/java/org/springframework/example/defaults/N.java
+++ spring-example/src/main/java/org/springframework/example/defaults/N.java
@@ -2,6 +2,6 @@ package org.springframework.example.defaults;
 
 import org.springframework.stereotype.Component;
 
-//@Component("nn")
+@Component("nn")
 public class N {
 }
