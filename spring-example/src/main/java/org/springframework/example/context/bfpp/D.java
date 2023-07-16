2b6a0bb78d6fea196f951ecaefda32ab4cfcd9ee
hanchen <m13699236245@163.com>
2023-07-16 11:11:57 +0800

    5-12. BeanDefinitionRegistryPostProcessor修改BeanDefinition的问题

--- spring-example/src/main/java/org/springframework/example/context/bfpp/D.java
+++ spring-example/src/main/java/org/springframework/example/context/bfpp/D.java
@@ -2,7 +2,9 @@ package org.springframework.example.context.bfpp;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.beans.factory.support.AbstractBeanDefinition;
 import org.springframework.beans.factory.support.BeanDefinitionBuilder;
 import org.springframework.beans.factory.support.BeanDefinitionRegistry;
 import org.springframework.beans.factory.support.BeanDefinitionRegistryPostProcessor;
@@ -26,5 +28,8 @@ public class D implements BeanDefinitionRegistryPostProcessor {
 
 		BeanDefinitionBuilder f = BeanDefinitionBuilder.genericBeanDefinition(F.class);
 		registry.registerBeanDefinition("f", f.getBeanDefinition());
+
+
+
 	}
 }
--- spring-example/src/main/java/org/springframework/example/context/bfpp/L.java
+++ spring-example/src/main/java/org/springframework/example/context/bfpp/L.java
@@ -2,37 +2,24 @@ package org.springframework.example.context.bfpp;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
-import org.springframework.beans.factory.support.AbstractBeanDefinition;
 import org.springframework.beans.factory.support.BeanDefinitionRegistry;
 import org.springframework.beans.factory.support.BeanDefinitionRegistryPostProcessor;
 import org.springframework.core.PriorityOrdered;
 import org.springframework.stereotype.Component;
 
 
-@Component
 @Slf4j(topic = "e")
-public class I implements BeanDefinitionRegistryPostProcessor, PriorityOrdered {
+public class L implements BeanDefinitionRegistryPostProcessor {
 
 	@Override
 	public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
-		log.debug("i-s scan parent postProcessBeanFactory PriorityOrdered");
+		log.debug("l-s scan parent postProcessBeanFactory ");
 	}
 
 	@Override
 	public void postProcessBeanDefinitionRegistry(BeanDefinitionRegistry registry) throws BeansException {
-		log.debug("i-s scan subclass postProcessBeanDefinitionRegistry PriorityOrdered");
-
-		AbstractBeanDefinition abstractBeanDefinition = (AbstractBeanDefinition) registry.getBeanDefinition("j");
-		abstractBeanDefinition.setBeanClass(L.class);
+		log.debug("l-s scan subclass postProcessBeanDefinitionRegistry ");
 	}
-
-
-	@Override
-	public int getOrder() {
-		return 0;
-	}
-
 
 }
