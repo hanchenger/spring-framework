0aaeed871279d4ad43eee214ba9d0f1aafb56270
hanchen <m13699236245@163.com>
2023-08-20 15:37:24 +0800

    7-02.模拟spring扫描

--- spring-example/src/test/java/org/springframework/example/scan/ScanTest.java
+++ spring-example/src/test/java/org/springframework/example/scan/ScanTest.java
@@ -4,8 +4,8 @@ package org.springframework.example.scan;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.example.lifeCycle.config.LifeCycleConfig;
 import org.springframework.example.scan.config.ScanConfig;
+import org.springframework.example.scan.util.ScanBeanDefinitionRegistryPostProcessor;
 
 @Slf4j(topic = "e")
 public class ScanTest {
@@ -15,5 +15,17 @@ public class ScanTest {
 	public void defaultScanTest(){
 		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(ScanConfig.class);
 	}
+
+	/**
+	 * 模拟扫描
+	 */
+	@Test
+	public void defaultImitateScanTest(){
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+		context.addBeanFactoryPostProcessor(new ScanBeanDefinitionRegistryPostProcessor());
+		context.refresh();
+
+	}
+
 
 }
---
+++ spring-example/src/test/java/org/springframework/example/scan/bean/F.java
@@ -0,0 +1,17 @@
+package org.springframework.example.scan.bean;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
+@Slf4j(topic = "e")
+public class F {
+
+
+	@PostConstruct
+	public void init(){
+		log.debug("f init");
+	}
+}
